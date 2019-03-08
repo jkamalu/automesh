72103e5053ade2cb97f0e7d025a73b238f48496e
jkamalu <john.kamalu@gmail.com>
2019-03-08 04:56:38 +0100

    Processing all user data. Abandoning efforts to use all channels in prediction.

--- data_utils.py
+++ data_utils.py
@@ -42,7 +42,11 @@ def process_lm3(fname):
             landmark = next(f, None)
             xyz_vals = next(f, None)
             if landmark and xyz_vals:
-                landmarks[LM_INDEX[landmark.rstrip()]] = np.array(xyz_vals.split()).astype(np.float32)
+                try:
+                    landmarks[LM_INDEX[landmark.rstrip()]] = np.array(xyz_vals.split()).astype(np.float32)
+                except KeyError:
+                    # rare ear and temple features
+                    continue
             else:
                 break
     return landmarks
@@ -82,16 +86,20 @@ def process_uid(uid_folder, E={"lm3", "bnt"}, C={"YR", "PR", "CR", "O"}):
     return data
 
 def load_data(path):
+    print("Loading data")
     lookup_table = {}
     id_pattern = re.compile(r"bs[0-9]{3}")
     uids = [f for f in os.listdir(path) if id_pattern.findall(f)]
-    for uid in sorted(uids):
+    for i, uid in enumerate(sorted(uids)):
+        if (i - 1) % 10 == 0 and i > 10:
+            print("Processed data for {} users".format(i - 1))
         uid_folder = os.path.join(path, uid)
         if not os.path.isdir(uid_folder): continue
         lookup_table[uid] = process_uid(uid_folder)
     return lookup_table
 
 def convert_data(lookup_table):
+    print("Converting data")
     n_samples = 0
     landmarks = []
     face_data = []
@@ -104,7 +112,40 @@ def convert_data(lookup_table):
             n_samples += 1
     return lookup_table, landmarks, face_data
 
+
+def analyze_shape(face_data, h_cut, w_cut):
+    shapes = np.array([d.shape[0:2] for d in face_data])
+
+    h_cut = 280
+    w_cut = 210
+
+    h_good = shapes[:, 0][shapes[:, 0] < h_cut]
+    w_good = shapes[:, 1][shapes[:, 1] < w_cut]
+    s_good = shapes[np.all(np.stack([shapes[:, 0] < h_cut, shapes[:, 1] < w_cut]), axis=0)]
+
+    print("{} ({}) at {} h_cut".format(h_good.shape[0], h_good.shape[0] / shapes.shape[0], h_cut))
+    print("{} ({}) at {} w_cut".format(w_good.shape[0], w_good.shape[0] / shapes.shape[0], w_cut))
+    print("{} ({}) at {} h_cut and {} w_cut".format(s_good.shape[0], s_good.shape[0] / shapes.shape[0], h_cut, w_cut))
+
+def trim_data(lookup_table, landmarks, face_data, h_cut, w_cut):
+    print("Trimming data")
+    D = {}
+    L = []
+    F = []
+    count = 0
+    for uid in lookup_table:
+        D[uid] = {}
+        for config in lookup_table[uid]:
+            idx = lookup_table[uid][config]
+            if face_data[idx].shape[0] < h_cut and face_data[idx].shape[1] < w_cut:
+                L.append(landmarks[idx])
+                F.append(face_data[idx])
+                D[uid][config] = count
+                count += 1
+    return D, L, F
+
 def padding_data(face_data):
+    print("Padding data")
     w_max = max(x.shape[1] for x in face_data)
     h_max = max(x.shape[0] for x in face_data)
     z_min = min(np.min(x) for x in face_data)
@@ -114,19 +155,24 @@ def padding_data(face_data):
         h_pad = (h_max - f.shape[0]) // 2
         f_pad = ((h_pad, h_max - f.shape[0] - h_pad), (w_pad, w_max - f.shape[1] - w_pad), (0, 0))
         face_data[i] = np.pad(f, f_pad, mode="constant", constant_values=z_min)
+    
+    return face_data
 
 def scaling_data(face_data):
+    """
+    Assumes (h.3D, w.3D, z.3D, h.2D, w.2D) channel ordering
+    """
+    print("Scaling data")
     N, W, H, channels = face_data.shape
 
     for i in range(face_data.shape[0]):
-        for channel in range(channels):
-            lowest, low = np.sort(np.unique(face_data[i, :, :, channel]))[:2]
-            face_data[i, :, :, channel][face_data[i, :, :, channel] == lowest] = low
+        lowest, low = np.sort(np.unique(face_data[i, :, :, 2]))[:2]
+        face_data[i, :, :, 2][face_data[i, :, :, 2] == lowest] = low
 
     scaler = MinMaxScaler()
-    scaler.fit(face_data[:, :, :, :].reshape(-1, channels))
-    scaled_z = scaler.transform(face_data[:, :, :, :].reshape(-1, channels)).reshape((N, W, H, channels))
-    face_data[:, :, :, :] = scaled_z
+    scaler.fit(face_data[:, :, :, 2].reshape(-1, 1))
+    scaled_z = scaler.transform(face_data[:, :, :, 2].reshape(-1, 1)).reshape((N, W, H))
+    face_data[:, :, :, 2] = scaled_z
     return scaler, face_data
 
 def visualize_z(face_data, z_channel=0, lookup_table=None, uid="bs000"):
