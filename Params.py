9fef07c2b9b0e478380e984c39730266b7eb28c7
John Kamalu <john.kamalu@gmail.com>
2019-03-10 15:04:32 +0000

    Training and visualization code for history. Custom metric. Param changing

--- Params.py
+++ Params.py
@@ -1,17 +1,16 @@
 class Params:
 
-    drop_rate = 0.4
+    drop_rate = 0.5
     n_filters = 32
     n_rand = 100
-    batch_size = 64
-    lr_D =  0.0001
+    batch_size = 32
+    lr_D =  0.0002
     lr_GD = 0.0002
-    real_l = 1
+    real_l = 0.9
     fake_l = 0
     steps_D = 1
-    steps_GD = 2
-    beta_1 = 0.5
+    steps_GD = 1
     
     def __init__(self, X, X_cond=None):
         _, self.W, self.H, self.n_channels = X.shape
-        _, self.n_cond = X_cond.shape if X_cond else (None, 0)
+        _, self.n_cond = X_cond.shape if X_cond is not None else (None, 0)
